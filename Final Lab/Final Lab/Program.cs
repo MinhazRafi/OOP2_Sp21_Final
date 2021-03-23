afeb37a341d0592cc7af1a815a4d17d39db14440
Minhazur Rahman <iamrafi404@gmail.com>
2021-03-23 12:46:34 -0400

    final lab-1

---
+++ Final Lab/Final Lab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Abdur Rahman", "1939", 65000);
+            Account a2 = new Account("Minhazur Rahman", "2001", 80000);
+            Account a3 = new Account("Radia Rahman", "2101", 58000);
+
+            a1.Withdraw(500);
+            a2.Withdraw(600);
+            a1.Transfer(a2, 3500);
+            a3.Deposit(5000);
+            a1.showAllTranscation();
+            a2.showAllTranscation();
+        }
+    }
+}
+
